c7dbc5f544f4c4d1996d26320f073702fa2ce96e
gyulab <gyulab@users.noreply.github.com>
2026-08-13 21:37:18 +0000

    deploy: c9af01355738ec09285eb2d9b82937e19e8a8569

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,18 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-building-an-agentic-workflow-for-tcad",
-        
-          title: "Building an Agentic Workflow for TCAD",
-        
-        description: "A practical guide for semiconductor researchers who want to use coding agents for repetitive TCAD setup, input checking, parameter sweeps, and reproducible reporting.",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/agentic-workflow-tcad/";
-          
-        },
-      },{id: "post-irps-2026-pinn-assisted-compact-modeling-for-nvcap-arrays",
+        },{id: "post-irps-2026-pinn-assisted-compact-modeling-for-nvcap-arrays",
         
           title: "IRPS 2026: PINN-Assisted Compact Modeling for nvCAP Arrays",
         
